a98869455e3d83e24684a0eb38011f3bb872ea7d
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-05-03 09:45:30 -0300

    adicionando arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+
+@Component({
+  selector:  'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent{
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor : number;
+  destino : number;
+
+  trasnsferir(){
+    console.log('Solicitado nova transferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir);
+
+
+  }
+
+}
